86a6aaccd0889f93efbc73b0e4c8846b1c8d1589
GabrielPerlo <gabrielperloestelastalitas@gmail.com>
2022-09-28 11:45:25 -0300

    tp1 ejercicio 2 hecho

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Tp1 from "./components/Tp1";
+import Tp2 from "./components/Tp2";
 
 function App() {
   return (
+    <>
     <Tp1></Tp1>
+    <Tp2 frase= 'mi amigo!'></Tp2>    
+    </>
   );
 }
 
---
+++ src/components/Tp2.jsx
@@ -0,0 +1,9 @@
+const Tp2 = (props) => {
+    return (
+        <div>
+            <h1>Hola {props.frase}</h1>
+        </div>
+    );
+};
+
+export default Tp2;
